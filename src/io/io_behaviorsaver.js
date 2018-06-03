abd655ab6a6debe8fce81726beabbd4e0fe91d00
Alireza <alireza.mixedreality@gmail.com>
2018-06-03 12:43:49 +0200

    feat: Update the behavior pkg on the robot on saveBehavior clicked  - It will create a SSH tunnel to the robot and overwrite the behavior package

--- src/io/io_behaviorsaver.js
+++ src/io/io_behaviorsaver.js
@@ -59,13 +59,89 @@ IO.BehaviorSaver = new (function() {
 		}
 	}
 
+    String.prototype.format = function() {
+        var formatted = this;
+        for( var arg in arguments ) {
+            formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+        }
+        return formatted;
+    };
+
 	var saveSuccessCallback = function() {
 		T.logInfo("Save successful!");
 		UI.Panels.Terminal.hide();
 		UI.Settings.updateBehaviorlib();
 		UI.Tools.notifyRosCommand('save');
-	}
+    }
 
+    var saveOnRobot = function() {
+        var names = Behavior.createNames();
+        var package_name = names.rosnode_name;
+        var ssh_username = UI.Settings.getRobotUsername();
+        var ssh_hostname = UI.Settings.getRobotHostname();
+        var ssh_package_path = ""
+
+        if(ssh_username === "" || ssh_hostname === "" /*|| ssh_password === ""*/) {
+            T.logError("SSH configurations are not well defined, fix them from Configuration Tab -> Code Generation Box");
+            return;
+        }
+
+        var ssh_password = window.prompt("Please enter {0}:{1} password: ".format(ssh_username, ssh_hostname));
+        if(ssh_password == null || ssh_password == "") {
+            return;
+        }
+
+        ROS.getPackagePath(package_name, (package_path) =>
+        {
+            try
+            {
+                var SSHClient = require('ssh2').Client;
+                conn = new SSHClient();
+                conn.on('ready', function() {
+                  var ros_distro = process.env.ROS_DISTRO;
+                  conn.exec('bash -c "source .profile && rospack find {1}"'.format(ros_distro, package_name), function(err, stream) {
+                    if (err) {
+                        T.logError(err);
+                    }
+                    stream.on('close', function(code, signal) {
+                      conn.end();
+                    })
+                    .on('data', function(remote_package_path) {
+                      ssh_package_path = remote_package_path;
+                      T.logInfo('Path to package on the robot: ' + ssh_package_path);
+                      conn.end()
+                      var scp_client = require('scp2')
+                      var ssh_query = '{0}:{1}@{2}:{3}/'.format(
+                          ssh_username,
+                          ssh_password,
+                          ssh_hostname,
+                          ssh_package_path).replace(/\s+/, "");
+                      scp_client.scp(package_path+"/", String(ssh_query), function(err) {
+                        var err_str = String(err);
+                        if( err_str != 'undefined') {
+                            T.logError("scp failed "+err);
+                        }
+                      });
+                    }).stderr.on('data', function(data) {
+                      T.logError(data+", Make sure robot's ROS environment is configured properly in \".profile\" script");
+                    })
+                  });
+                })
+                .on('error', function(err) {
+                        T.logError(err);
+                        T.logError("Please check your ssh configuration and entered password.");
+                 })
+                 .connect({
+                  host: ssh_hostname,
+                  username: ssh_username,
+                  password: ssh_password
+                });
+            }
+            catch(ex) {
+                T.logError("An exception occurred: "+ex.message+", Make sure you installed the dependencies properly!")
+            }
+        })
+    }
 
 	this.saveStateMachine = function() {
 		T.clearLog();
@@ -97,8 +173,12 @@ IO.BehaviorSaver = new (function() {
 				// make sure code file exists before creating the manifest
 				// this reduces the risk for orphan manifests
 				storeBehaviorManifest(generated_manifest, () => {
-					saveSuccessCallback();
-				});
+                    if(UI.Settings.isSaveOnRobotEnabled()) {
+                        //Save/Overwrite the behavior on the robot
+                        saveOnRobot();
+                    }
+                    saveSuccessCallback();
+        		});
 			});
 		});
 	}
